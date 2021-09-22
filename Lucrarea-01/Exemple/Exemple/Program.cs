bb1ca58591f39a8506b396752f171e8512644503
Daniel Iercan <daniel.iercan@theaccessgroup.com>
2021-09-22 22:36:58 +0300

    Start L01

---
+++ Lucrarea-01/Exemple/Exemple/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exemple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
