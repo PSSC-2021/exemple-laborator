7ab1c44f3b486ecf615b65221e65c66908312bde
Daniel Iercan <daniel.iercan@theaccessgroup.com>
2021-10-30 10:03:22 +0300

    Option and try

--- Lucrarea-03/Exemple/Exemple/Program.cs
+++ Lucrarea-03/Exemple/Exemple/Program.cs
@@ -6,6 +6,8 @@ using static Exemple.Domain.ExamGradesOperation;
 using Exemple.Domain;
 using System.Threading.Tasks;
 using LanguageExt;
+using static LanguageExt.Prelude;
+using System.Net.Http;
 
 namespace Exemple
 {
@@ -15,6 +17,28 @@ namespace Exemple
 
         static async Task Main(string[] args)
         {
+            //var testNumber = StudentRegistrationNumber.TryParse("LM12345");
+            //var studentExists = await testNumber.Match(
+            //    Some: testNumber => CheckStudentExists(testNumber).Match(Succ: value => value, exception=>false),
+            //    None: () => Task.FromResult(false)
+            //);
+
+            //var result = from studentNumber in StudentRegistrationNumber.TryParse("LM12345")
+            //                                        .ToEitherAsync(() => "Invlid student registration number.")
+            //             from exists in CheckStudentExists(studentNumber)
+            //                                        .ToEither(ex =>
+            //                                        {
+            //                                            Console.Error.WriteLine(ex.ToString());
+            //                                            return "Could not validate student reg. number";
+            //                                        })
+            //             select exists;
+
+            //await result.Match(
+            //     Left: message => Console.WriteLine(message),
+            //     Right: flag => Console.WriteLine(flag));
+
+
+
             var listOfGrades = ReadListOfGrades().ToArray();
             PublishGradesCommand command = new(listOfGrades);
             PublishGradeWorkflow workflow = new();
@@ -35,9 +59,30 @@ namespace Exemple
                 );
         }
 
+        private static int Sum()
+        {
+            Option<int> two = Some(2);
+            Option<int> four = Some(4);
+            Option<int> six = Some(6);
+            Option<int> none = None;
+
+            var result = from x in two
+                         from y in four
+                         from z in six
+                         from n in none
+                         select x + y + z + n;
+            // This expression succeeds because all items to the right of 'in' are Some of int
+            // and therefore it lands in the Some lambda.
+            int r = match(result,
+                           Some: v => v * 2,
+                           None: () => 0);     // r == 24
+
+            return r;
+        }
+
         private static List<UnvalidatedStudentGrade> ReadListOfGrades()
         {
-            List <UnvalidatedStudentGrade> listOfGrades = new();
+            List<UnvalidatedStudentGrade> listOfGrades = new();
             do
             {
                 //read registration number and grade and create a list of greads
@@ -59,7 +104,7 @@ namespace Exemple
                     break;
                 }
 
-                listOfGrades.Add(new (registrationNumber, examGrade, activityGrade));
+                listOfGrades.Add(new(registrationNumber, examGrade, activityGrade));
             } while (true);
             return listOfGrades;
         }
@@ -70,6 +115,18 @@ namespace Exemple
             return Console.ReadLine();
         }
 
-        private static TryAsync<bool> CheckStudentExists(StudentRegistrationNumber student) => async () => true;
+        private static TryAsync<bool> CheckStudentExists(StudentRegistrationNumber student) {
+            Func<Task<bool>> func = async () =>
+                 {
+                     //HttpClient client = new HttpClient();
+
+                     //var response = await client.PostAsync($"www.university.com/checkRegistrationNumber?number={student.Value}", new StringContent(""));
+
+                     //response.EnsureSuccessStatusCode(); //200
+
+                     return true;
+                 };
+            return TryAsync(func);
+        }
     }
 }
